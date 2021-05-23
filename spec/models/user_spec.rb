0070ff564dad9ed440576b4bd111526a1cf1c439
2bo <developer.2bo@gmail.com>
2021-05-23 18:58:52 +0900

    exec rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   it 'has a valid factory' do
     expect(FactoryBot.build(:user)).to be_valid
   end
@@ -37,14 +36,14 @@ RSpec.describe User, type: :model do
   end
 
   it 'is invalid with a duplicate email address' do
-    FactoryBot.create(:user, email: "aaron@example.com")
-    user = FactoryBot.build(:user, email: "aaron@example.com")
+    FactoryBot.create(:user, email: 'aaron@example.com')
+    user = FactoryBot.build(:user, email: 'aaron@example.com')
     user.valid?
     expect(user.errors[:email]).to include('has already been taken')
   end
 
   it "returns a user's full name as a string" do
-    user = FactoryBot.build(:user, first_name:'John', last_name: 'Doe')
+    user = FactoryBot.build(:user, first_name: 'John', last_name: 'Doe')
     expect(user.name).to eq 'John Doe'
   end
 end
