677ef48f603f153628fb89692b19586553d63a56
2bo <developer.2bo@gmail.com>
2021-05-23 15:59:53 +0900

    p.55

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
   it 'is valid witha a first name, last name, email, and password' do
     user = described_class.new(
       first_name: 'Aaron',
@@ -14,43 +19,32 @@ RSpec.describe User, type: :model do
   end
 
   it 'is invalid without a frist name' do
-    user = described_class.new(first_name: nil)
+    user = FactoryBot.build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
   it 'is invalid without af last name' do
-    user = described_class.new(last_name: nil)
+    user = FactoryBot.build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
   it 'is invalide without an email address' do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
   end
 
   it 'is invalid with a duplicate email address' do
-    described_class.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'tester@example.com',
-      password: 'bottle-nouveau-pavilion-gights-furze'
-    )
-    user = described_class.new(
-      first_name: 'Jane',
-      last_name: 'Tester',
-      email: 'tester@example.com',
-      password: 'bottle-nouveau-pavilion-gights-furze'
-    )
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
     user.valid?
     expect(user.errors[:email]).to include('has already been taken')
   end
 
   it "returns a user's full name as a string" do
-    user = described_class.new(
-      first_name: 'John',
-      last_name: 'Doe',
-      email: 'johndoe@example.com'
-    )
+    user = FactoryBot.build(:user, first_name:'John', last_name: 'Doe')
     expect(user.name).to eq 'John Doe'
   end
 end
