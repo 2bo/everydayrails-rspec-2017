3f9874ffaa131fc9c5ca8a69993e1c12636a805c
2bo <developer.2bo@gmail.com>
2021-06-06 11:38:45 +0900

    p.116

--- spec/requests/projects_api_spec.rb
+++ spec/requests/projects_api_spec.rb
@@ -31,4 +31,21 @@ RSpec.describe 'ProjectApis', type: :request do
 
     expect(json['name']).to eq 'Second Sample Project'
   end
+
+  it 'creates a project' do
+    # uesr作成
+    user = FactoryBot.create(:user)
+    # project_attributeを作成
+    project_attributes = FactoryBot.attributes_for(:project)
+    # apiにアクセス
+    expect do
+      post api_projects_path, params: {
+        user_email: user.email,
+        user_token: user.authentication_token,
+        project: project_attributes
+      }
+    end.to change(user.projects, :count).by(1)
+    # createdが帰っていることを確認
+    expect(response).to have_http_status(:success)
+  end
 end
