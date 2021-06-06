c7e85b04b655beb32a4b7a6cabca9cdef1caa2f9
2bo <developer.2bo@gmail.com>
2021-06-06 13:00:11 +0900

    until p.127

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -5,7 +5,8 @@ require 'rails_helper'
 RSpec.describe 'Projects', type: :feature do
   it 'user creates a new project' do
     user = FactoryBot.create(:user)
-    sign_in_as user
+    sign_in user
+    visit root_path
 
     expect do
       click_link 'New Project'
